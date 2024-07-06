bc1615da7e383b0d821419494665817eeadc21de
Yu-Chun Lin <count2841@gmail.com>
2024-07-06 15:36:58 +0800

    Add chrt FIFO scheduling to benchmark script
    - Add `chrt` with FIFO scheduling to improve the performance of the
    benchmarking executions.
    - Add an argument for users to set FIFO priority.

--- test/Readme.md
+++ test/Readme.md
@@ -5,11 +5,11 @@
 1. Use `make` to generate executable files.  
 2. Run the following command to generate performance results:  
    ```shell
-   ./run_perf.sh {RUN_MODE(0)} {NUM_READERS} {NUM_WRITERS} {VALID_CPUS} {OUTPUT_DIR}
+   ./run_perf.sh {RUN_MODE(0)} {NUM_READERS} {NUM_WRITERS} {VALID_CPUS} {OUTPUT_DIR} {FIFO_PRIORITY}
    ```
    Example:
    ```shell
-   ./run_perf.sh 0 7 1 0,2,4,6 perf_output
+   ./run_perf.sh 0 7 1 0,2,4,6 perf_output 90
    ```
    This generates performance results for 1 writer and 7 readers using different methods (urcu + ctp).
 
@@ -17,10 +17,10 @@
 1. Use `make` to generate executable files.
 2. Run the following command to generate performance results:
    ```sh
-   ./run_perf.sh {RUN_MODE(1)} {NUM_READERS} {NUM_WRITERS} {VALID_CPUS} {OUTPUT_DIR}
+   ./run_perf.sh {RUN_MODE(1)} {NUM_READERS} {NUM_WRITERS} {VALID_CPUS} {OUTPUT_DIR} {FIFO_PRIORITY}
    ```
    Example:
    ```sh
-   ./run_perf.sh 1 11 1 0,2,4,6 perf_output
+   ./run_perf.sh 1 100 1 0,2,4,6 perf_output 90
    ```
-   This generates different combinations of 1 writer and multiple readers (1~11) using different methods (5 urcu + 2 ctp).
+   This generates different combinations of 1 writer and multiple readers [1, 100] using different methods (5 urcu + 2 ctp).
--- test/run_perf.sh
+++ test/run_perf.sh
@@ -6,6 +6,7 @@ NUM_READERS=$2
 NUM_WRITERS=$3
 VALID_CPUS=$4
 OUTPUT_DIR=$5
+FIFO_PRIORITY=$6
 
 # Executable binaries
 EXECUTABLES=(
@@ -30,14 +31,14 @@ for EXECUTABLE in "${EXECUTABLES[@]}"; do
         # Use NUM_READERS directly if RUN_MODE is 0
         OUTPUT_FILE="${OUTPUT_DIR}/perf_output_${EXECUTABLE}_${NUM_READERS}_readers.txt"
         OUTPUT_TXT="${OUTPUT_DIR}/csv/${EXECUTABLE}_${NUM_READERS}_${NUM_WRITERS}.txt"
-        taskset -c $VALID_CPUS perf stat -e cycles,instructions,context-switches,cpu-migrations,L1-dcache-loads,L1-dcache-stores,cache-references,cache-misses,L1-dcache-load-misses,L1-dcache-store-misses,LLC-load-misses,LLC-store-misses \
+        sudo taskset -c $VALID_CPUS chrt -f $FIFO_PRIORITY perf stat -e cycles,instructions,context-switches,cpu-migrations,L1-dcache-loads,L1-dcache-stores,cache-references,cache-misses,L1-dcache-load-misses,L1-dcache-store-misses,LLC-load-misses,LLC-store-misses \
                 -o $OUTPUT_FILE -- ./$EXECUTABLE $NUM_READERS $NUM_WRITERS $VALID_CPUS > $OUTPUT_TXT 2>&1
     else
         # Run for each reader count from 1 to NUM_READERS
         for ((NUM_READER=1; NUM_READER<=NUM_READERS; NUM_READER++)); do
             OUTPUT_FILE="${OUTPUT_DIR}/perf_output_${EXECUTABLE}_${NUM_READER}_readers.txt"
             OUTPUT_TXT="${OUTPUT_DIR}/csv/${EXECUTABLE}_${NUM_READER}_${NUM_WRITERS}.txt"
-            taskset -c $VALID_CPUS perf stat -e cycles,instructions,context-switches,cpu-migrations,L1-dcache-loads,L1-dcache-stores,cache-references,cache-misses,L1-dcache-load-misses,L1-dcache-store-misses,LLC-load-misses,LLC-store-misses \
+            sudo taskset -c $VALID_CPUS chrt -f $FIFO_PRIORITY perf stat -e cycles,instructions,context-switches,cpu-migrations,L1-dcache-loads,L1-dcache-stores,cache-references,cache-misses,L1-dcache-load-misses,L1-dcache-store-misses,LLC-load-misses,LLC-store-misses \
                     -o $OUTPUT_FILE -- ./$EXECUTABLE $NUM_READER $NUM_WRITERS $VALID_CPUS > $OUTPUT_TXT 2>&1
         done
     fi
